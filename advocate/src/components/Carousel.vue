921a533425921f136d05b89a1d3f081a0c9e5de4
Joy <greeniejoy@gmail.com>
2021-06-13 23:36:11 -0500

    made carousel component with mock data

---
+++ advocate/src/components/Carousel.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-carousel hide-delimiters cycle height="200" width="200" show-arrows-on-hover :show-arrows="false">
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+    ></v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+export default {
+  name: "Carousel",
+  props: {
+
+  },
+  data () {
+      return {
+       items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+          },
+        ],
+
+      }
+
+  }
+};
+</script>
